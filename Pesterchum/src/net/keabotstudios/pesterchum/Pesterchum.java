b142441dc585d138d3a6333b7c9a4315a4047a3d
Richard Abbott <richie0415@hotmail.com>
2016-04-20 21:02:52 -0400

    Initial Commit

---
+++ Pesterchum/src/net/keabotstudios/pesterchum/Pesterchum.java
@@ -0,0 +1,13 @@
+package net.keabotstudios.pesterchum;
+
+import net.keabotstudios.pesterchum.server.Server;
+
+public class Pesterchum {
+
+	public static void main(String[] args) {
+		if(args.length > 0 && args[0] == "server") {
+			Server.launch();
+		}
+	}
+
+}
---
+++ Pesterchum/src/net/keabotstudios/pesterchum/server/Server.java
@@ -0,0 +1,9 @@
+package net.keabotstudios.pesterchum.server;
+
+public class Server {
+	
+	public static void launch() {
+		
+	}
+
+}
