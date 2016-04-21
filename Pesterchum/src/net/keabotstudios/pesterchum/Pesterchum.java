e978a8de146367ba044383e501e6c3c5667b6b4a
Richard Abbott <richie0415@hotmail.com>
2016-04-20 21:08:36 -0400

    EXTREMELY basic stuffs

--- Pesterchum/src/net/keabotstudios/pesterchum/Pesterchum.java
+++ Pesterchum/src/net/keabotstudios/pesterchum/Pesterchum.java
@@ -6,7 +6,10 @@ public class Pesterchum {
 
 	public static void main(String[] args) {
 		if(args.length > 0 && args[0] == "server") {
-			Server.launch();
+			Server.launch(); // START PESTERCHUM SERVER
+			return;
+		} else {
+			// START PESTERCHUM CLIENT
 		}
 	}
 
